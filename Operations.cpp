fc49bb066be0d9eab60dbfaf00e04a8e3e08b01b
mabrutsaunierVadim <vadimmabrutsaunier@gmail.com>
2024-03-11 14:19:17 +0100

    modification
    modification des fichiers avec l'ajout des fonctions multiplication et division

--- Operations.cpp
+++ Operations.cpp
@@ -17,3 +17,21 @@ int soustraction(int a, int b)
 	
 	return resultat;
 }
+
+int multiplication(int a, int b)
+{
+	int resultat;
+	
+	resultat = a * b;
+	
+	return resultat;
+}
+
+float division(float a, float b)
+{
+	float resultat;
+	
+	resultat = a / b;
+	
+	return resultat;
+}
--- Operations.h
+++ Operations.h
@@ -1,3 +1,7 @@
 int addition(int a, int b);
 
 int soustraction(int a, int b);
+
+int multiplication(int a, int b);
+
+float division(float a, float b);
